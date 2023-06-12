4a1cc6b4094263a8f4ce7da79a50415f1ebeec51
Tatiana Safronova <117063183+mrclvck@users.noreply.github.com>
2023-06-12 19:24:13 +0300

    Update UserAuthentication.java

--- src/main/java/org/example/src/database/UserAuthentication.java
+++ src/main/java/org/example/src/database/UserAuthentication.java
@@ -5,77 +5,67 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Random;
 
-/**
- * The type User authentication.
- */
 public class UserAuthentication {
     private static String currentUser = null;
-
-    /**
-     * Gets current user.
-     *
-     * @return the current user
-     */
     public static String getCurrentUser() {
         return currentUser;
     }
-
-    /**
-     * User authentication.
-     */
-    public static void userAuthentication() {
-        System.out.println("Введите имя пользователя");
-        Scanner scanner = new Scanner(System.in);
-        String login = scanner.nextLine().trim();
-        if (login.contains("'")) throw new NoSuchElementException();
-        try {
-            if (DatabaseConnection.executePreparedStatement("SELECT * FROM USERS WHERE login = ?", login).next()) {
-                userLoggingIn(login, scanner);
-            } else {
-                System.out.println("Пользователя с таким именем не существует, если хотите зарегистрироваться, введите 1, если выйти - нажмите любую клавишу");
-                if (scanner.nextLine().trim().equals("1")) userRegistration(login, scanner);
-            }
-        } catch (SQLException | NullPointerException e) {
-            System.out.println(e.getMessage());
-        }
+    public static void logOut() {
+        currentUser = null;
     }
-    private static void userRegistration(String login, Scanner scanner) {
-        System.out.println("Введите пароль");
-        MessageDigest md;
+    public static boolean userRegistration(String login, String password) {
         try {
-            md = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        String salt = saltGetter();
-        byte[] hash = md.digest(("*63&^mVLC(#" + scanner.nextLine().trim() + salt).getBytes(StandardCharsets.UTF_8));
-        DatabaseConnection.executePreparedStatement("INSERT INTO USERS (login, hash, salt) VALUES (?, ?, ?)", login, Arrays.toString(hash), salt);
-        System.out.println("Вы успешно прошли регистрацию");
-        currentUser = login;
-    }
-    private static void userLoggingIn(String login, Scanner scanner) {
-        boolean userTrying = true;
-        while (userTrying) {
-            System.out.println("Введите пароль");
             try {
-                MessageDigest md = MessageDigest.getInstance("SHA-1");
-                ResultSet resultSetSalt = DatabaseConnection.executePreparedStatement("SELECT salt FROM USERS WHERE login = ?", login);
-                resultSetSalt.next();
-                ResultSet resultSetHash = DatabaseConnection.executePreparedStatement("SELECT hash FROM USERS WHERE login = ?", login);
-                resultSetHash.next();
-                byte[] hash = md.digest(("*63&^mVLC(#" + scanner.nextLine().trim() + resultSetSalt.getString(1)).getBytes(StandardCharsets.UTF_8));
-                if (Arrays.toString(hash).equals(resultSetHash.getString(1))) {
-                    System.out.println("Вы успешно прошли аутентификацию");
-                    currentUser = login;
-                    userTrying = false;
+                ResultSet resultSet = DatabaseConnection.executePreparedStatement("SELECT * FROM USERS WHERE login = ?", login);
+                if (resultSet.next()) {
+                    return false;
                 } else {
-                    System.out.println("Неверный пароль (если хотите выйти из программы, вызовите EOF, нажав control + d)");
+                    resultSet.close();
+                    throw new IllegalArgumentException();
                 }
-            } catch (SQLException | NoSuchAlgorithmException | NullPointerException e) {
+            } catch (IllegalArgumentException e) {
+                try {
+                    MessageDigest md = MessageDigest.getInstance("SHA-1");
+                    String salt = saltGetter();
+                    byte[] hash = md.digest(("*63&^mVLC(#" + password + salt).getBytes(StandardCharsets.UTF_8));
+                    DatabaseConnection.executePreparedStatement("INSERT INTO USERS (login, hash, salt) VALUES (?, ?, ?)", login, Arrays.toString(hash), salt).close();
+                } catch (NoSuchAlgorithmException e1) {
+                    System.out.println(e1.getMessage());
+                } catch (IllegalArgumentException e1) {
+                    return true;
+                }
+                return false;
+            } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
+            return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static boolean userLoggingIn(String login, String  password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            ResultSet resultSetSalt = DatabaseConnection.executePreparedStatement("SELECT salt FROM USERS WHERE login = ?", login);
+            resultSetSalt.next();
+            ResultSet resultSetHash = DatabaseConnection.executePreparedStatement("SELECT hash FROM USERS WHERE login = ?", login);
+            resultSetHash.next();
+            byte[] hash = md.digest(("*63&^mVLC(#" + password + resultSetSalt.getString(1)).getBytes(StandardCharsets.UTF_8));
+            resultSetSalt.close();
+            if (Arrays.toString(hash).equals(resultSetHash.getString(1))) {
+                currentUser = login;
+                resultSetHash.close();
+                return true;
+            } else {
+                resultSetHash.close();
+                return false;
+            }
+        } catch (SQLException | NoSuchAlgorithmException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
     }
     private static String saltGetter() {
